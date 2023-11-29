1f8d1c2d3348cd2f4d2d311ec1af8ff5f2010131
Henry Hale <henryharrison8117@gmail.com>
2023-11-29 14:02:06 -0600

    refactor: auto detect dev mode, no more dev option

--- unit.js/index.js
+++ unit.js/index.js
@@ -1,17 +1,16 @@
-export function render(component, root, dev = true) {
+export function render(component, root) {
     root = root instanceof HTMLElement ? root : document.querySelector(root);
 
     const app = component();
 
     root.innerHTML = app.html();
+    console.log(app.html());
     
-    if (dev) {
-        if (import.meta.hot) {
-            import.meta.hot.on("unit:update", ({ fn, contents, file }) => {
-                console.log("[vite] hot updated: ", file);
-                app.setFn(fn, contents);
-                root.innerHTML = app.html();
-            });
-        }
+    if (import.meta.hot) {
+        import.meta.hot.on("unit:update", ({ fn, contents, file }) => {
+            console.log("[vite] hot updated: ", file);
+            app._setFn(fn, contents);
+            root.innerHTML = app.html();
+        });
     }
 }
